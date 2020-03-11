cc3b05fc53a1cdaffcafeab00436a53cb9829c3b
Ege Erdogan <ege@erdogan.dev>
2020-03-11 09:58:03 +0300

    array struct

--- helpers.h
+++ helpers.h
@@ -2,18 +2,17 @@
 #include <stdio.h>
 #include <regex.h>
 
+struct array_struct {
+  int count;
+  char *array[500];
+};
+
 int match_count(char *start_pattern) {
-
-}
-
-void list_commands(char *start_pattern) {
   regex_t regex;
   int reti;
   int match_count = 0;
-  char pattern[100] = "";
 
-  sprintf(pattern, "^%s", start_pattern);
-  regcomp(&regex, pattern, 0);
+  regcomp(&regex, start_pattern, 0);
 
   DIR *dir;
   struct dirent *ent;
@@ -21,10 +20,40 @@ void list_commands(char *start_pattern) {
   while ((ent = readdir (dir)) != NULL) {
     if (!regexec(&regex, ent->d_name, 0, NULL, 0)) {
       match_count++;
-      printf ("%s\n", ent->d_name);
     }
   }
   closedir (dir);
   regfree(&regex);
+
+  return match_count;
+}
+
+struct array_struct list_commands(char *start_pattern) {
+  int count;
+  regex_t regex;
+  int reti;
+  char pattern[100] = "";
+
+  struct array_struct result;
+  int i = 0;
+
+  sprintf(pattern, "^%s", start_pattern);
+  regcomp(&regex, pattern, 0);
+  count = match_count(pattern);
+
+  DIR *dir;
+  struct dirent *ent;
+  dir = opendir("/usr/bin");
+  while ((ent = readdir (dir)) != NULL) {
+    if (!regexec(&regex, ent->d_name, 0, NULL, 0)) {
+      result.array[i] = ent->d_name;
+      i++;
+    }
+  }
+  closedir (dir);
+  regfree(&regex);
+
+  result.count = match_count;
+  return result;
 }
 
